d57ae2f72611adad47f2fa042afd44da556ed300
GiantLuigi4 <giantluigi4@users.noreply.github.com>
2021-06-23 16:09:04 -0400

    clean up method load function

--- native_vm/ConsoleApplication1/ConsoleApplication1/Method.cpp
+++ native_vm/ConsoleApplication1/ConsoleApplication1/Method.cpp
@@ -51,7 +51,7 @@ void Method::load(string name, string desc, string text, bool isPublic, bool isS
 		if (b == -4 || b == -7 || b == -8 || b == -14 || b == -15 || b == -16 || b == -17 || b == -18 || b == -19 || b == -20 || b == -21 || b == -22 || b == -26 || b == -27 || b == -28 || b == -30) {
 			tempB = b;
 			isFirst = true;
-			//			} else if (b == -5 || b == -6 || b == -9 || b == -10 || b == -11 || b == -12 || b == -13) {
+//			} else if (b == -5 || b == -6 || b == -9 || b == -10 || b == -11 || b == -12 || b == -13) {
 		} else if (b != -3) {
 			Instruction insn = Instruction();
 			insn.id = b;
@@ -60,22 +60,20 @@ void Method::load(string name, string desc, string text, bool isPublic, bool isS
 	}
 	this->isStatic = isStatic;
 	this->isPublic = isPublic;
-	string str = "";
-	for (Instruction i : insns) {
-		str += (char)i.id;
-		cout << (int) i.id;
-		if (i.ainfo0.length() > 0) str += i.ainfo0;
-		if (i.ainfo1.length() > 0) {
-			str += (char) -3;
-			str += i.ainfo1;
-		}
-	}
-	cout << str + "\n";
-
-	ofstream myfile1;
-	myfile1.open("method.txt");
-	myfile1 << str;
-	myfile1.close();
-
-//	instructions = insns.toArray(new Instruction[0]);
+//	string str = "";
+//	for (Instruction i : insns) {
+//		str += (char)i.id;
+//		cout << (int) i.id;
+//		if (i.ainfo0.length() > 0) str += i.ainfo0;
+//		if (i.ainfo1.length() > 0) {
+//			str += (char) -3;
+//			str += i.ainfo1;
+//		}
+//	}
+//	cout << str + "\n";
+//
+//	ofstream myfile1;
+//	myfile1.open("method.txt");
+//	myfile1 << str;
+//	myfile1.close();
 }
